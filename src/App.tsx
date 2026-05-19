2fdbf08e5dd5638903b3daa25c52332065c4d140
Minh Tam <ph.minhtam.0611@gmail.com>
2026-05-19 09:08:42 +0700

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { Routes } from 'react-router-dom'
+
 function App() {
   return (
-    <div className="min-h-screen bg-background">
-      <div className="max-w-[1440px] mx-auto px-margin">
-        <h1 className="text-headline-xl text-on-surface py-lg">
-          Calendar Task Planner
-        </h1>
+    <div className='min-h-screen bg-background'>
+      <div className='max-w-360 mx-auto px-margin'>
+        <h1 className='text-headline-xl text-on-surface py-lg'>Calendar Task Planner</h1>
+        <Routes>
+          {/* Add your routes here */}
+        </Routes>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
